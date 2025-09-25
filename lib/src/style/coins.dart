85a7d971266e6b49cb377aead714fed20ff02c7e
PhillipWong0627 <phillipwong0627@gmail.com>
2025-09-25 17:45:35 +0800

    fix: remove unused icon

--- lib/src/style/coins.dart
+++ lib/src/style/coins.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-enum CoinIcon { generic, btc, eth, base }
+enum CoinIcon { generic, btc, eth, base, xrp }
 
 Widget coinIcon(CoinIcon c) {
   switch (c) {
@@ -12,6 +12,8 @@ Widget coinIcon(CoinIcon c) {
       return SvgPicture.asset('assets/coins/eth-logo.svg');
     case CoinIcon.base:
       return Image.asset('assets/coins/base.png');
+    case CoinIcon.xrp:
+      return SvgPicture.asset('assets/coins/xrp-logo.svg');
     case CoinIcon.generic:
       return Image.asset('assets/coins/icon-tux-dark.png');
   }
